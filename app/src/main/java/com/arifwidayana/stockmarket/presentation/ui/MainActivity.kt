6275938f0af98ae6ddbd85b492815008a7d57250
Arif Widayana <arifwidayana@gmail.com>
2023-06-11 12:48:20 +0700

    feat: setup destination nav host with single activity

---
+++ app/src/main/java/com/arifwidayana/stockmarket/presentation/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.arifwidayana.stockmarket.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.arifwidayana.stockmarket.ui.theme.StockMarketTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
+        }
+    }
+}
