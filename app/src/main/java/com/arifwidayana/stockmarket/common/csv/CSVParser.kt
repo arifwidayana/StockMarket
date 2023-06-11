607efdb3c4f3fa912e457c1b9faba4338b81c7ba
Arif Widayana <arifwidayana@gmail.com>
2023-06-11 12:40:24 +0700

    feat: add csv parser

---
+++ app/src/main/java/com/arifwidayana/stockmarket/common/csv/CSVParser.kt
@@ -0,0 +1,70 @@
+package com.arifwidayana.stockmarket.common.csv
+
+import com.arifwidayana.stockmarket.data.mapper.toIntraDayInfo
+import com.arifwidayana.stockmarket.data.network.model.CompanyListing
+import com.arifwidayana.stockmarket.data.network.model.IntraDayInfo
+import com.arifwidayana.stockmarket.data.network.model.dto.IntraDayInfoDto
+import com.opencsv.CSVReader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.time.LocalDate
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface CSVParser<T> {
+    suspend fun parse(stream: InputStream): List<T>
+}
+
+@Singleton
+class CompanyListingsParser @Inject constructor(): CSVParser<CompanyListing> {
+    override suspend fun parse(stream: InputStream): List<CompanyListing> {
+        val csvReader = CSVReader(InputStreamReader(stream))
+        return withContext(Dispatchers.IO) {
+            csvReader
+                .readAll()
+                .drop(1)
+                .mapNotNull { line ->
+                    val symbol = line.getOrNull(0)
+                    val name = line.getOrNull(1)
+                    val exchange = line.getOrNull(2)
+                    CompanyListing(
+                        name = name ?: return@mapNotNull null,
+                        symbol = symbol ?: return@mapNotNull null,
+                        exchange = exchange ?: return@mapNotNull null
+                    )
+                }
+                .also {
+                    csvReader.close()
+                }
+        }
+    }
+}
+
+@Singleton
+class IntraDayInfoParser @Inject constructor(): CSVParser<IntraDayInfo> {
+    override suspend fun parse(stream: InputStream): List<IntraDayInfo> {
+        val csvReader = CSVReader(InputStreamReader(stream))
+        return withContext(Dispatchers.IO) {
+            csvReader
+                .readAll()
+                .drop(1)
+                .mapNotNull { line ->
+                    val timestamp = line.getOrNull(0) ?: return@mapNotNull null
+                    val close = line.getOrNull(4) ?: return@mapNotNull null
+                    val dto = IntraDayInfoDto(timestamp, close.toDouble())
+                    dto.toIntraDayInfo()
+                }
+                .filter {
+                    it.date.dayOfMonth == LocalDate.now().minusDays(4).dayOfMonth
+                }
+                .sortedBy {
+                    it.date.hour
+                }
+                .also {
+                    csvReader.close()
+                }
+        }
+    }
+}
