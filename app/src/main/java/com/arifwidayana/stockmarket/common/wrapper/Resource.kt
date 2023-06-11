897cef0bc300f5dfa6b3af0f04cb356ec232e00c
Arif Widayana <arifwidayana@gmail.com>
2023-06-11 12:40:09 +0700

    feat: add wrapper resource

---
+++ app/src/main/java/com/arifwidayana/stockmarket/common/wrapper/Resource.kt
@@ -0,0 +1,7 @@
+package com.arifwidayana.stockmarket.common.wrapper
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+    class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
+}
